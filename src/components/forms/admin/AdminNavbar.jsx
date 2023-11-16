4b97c6e1a8bf2122363ce63ea8b5da604bc60bbb
José Luis <joseluis.rodriguez@hotmail.es>
2023-11-16 23:36:40 +0100

    logo admin

--- src/components/forms/admin/AdminNavbar.jsx
+++ src/components/forms/admin/AdminNavbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Logo from '../../../media/logoNG.png'
 
 
 function AdminNavbar() {
   return (
     <div className="flex items-center py-2 md:py-4 min-h-16 w-full bg-base-100bg-base-100 justify-between">
-      <a className="btn btn-ghost text-xl">daisyUI</a>
+      <a href="/admin"><img src={Logo} width="150px" alt="aquacivis" /></a>
       <ul className="flex flex-row gap-4">
         <li>
         <button className="btn btn-outline btn-error">Cerrar sesión</button>
