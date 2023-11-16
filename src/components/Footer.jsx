9ee07e3142f98ca8beb797ae2499438288249255
José Luis <joseluis.rodriguez@hotmail.es>
2023-11-16 22:14:18 +0100

    logo y bg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import image from '../../public/drought_icon.png'
+import Logo from '../media/logoBN.png'
 
 export const Footer = () => {
 
   return (
     <footer className="footer items-center p-4 bg-neutral text-neutral-content">
       <aside className="flex justify-end items-center w-full">
-        <span className="text-slate-100 text-3xl font-bold tracking-wide">AQUACIVIS</span>
+        <a href="/"><img width="150px" src={Logo} alt="logo" /></a>
       </aside>
     </footer>
   )
